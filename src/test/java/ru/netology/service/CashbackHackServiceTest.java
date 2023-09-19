955cdf423f16ca0f8ef5fb1589ab80b5833a078f
ParadoX <djei007@mail.ru>
2023-09-19 12:27:22 +0300

    TestingNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void calculateServiceInTheBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1350;
+
+        int actual = service.remain(amount);
+        int expected = 650;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void calculateServiceNotInTheBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+        System.out.println(actual);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void calculateServiceInTheGradle() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+}
